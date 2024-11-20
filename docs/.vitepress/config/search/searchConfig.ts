2311405d0925727d419ce97658f2a65b9065d987
shenlink <2024133718@qq.com>
2024-11-20 22:57:33 +0800

    修改搜索配置，加上中文翻译

--- docs/.vitepress/config/search/searchConfig.ts
+++ docs/.vitepress/config/search/searchConfig.ts
@@ -4,5 +4,21 @@ type SearchConfig = | { provider: 'local'; options?: DefaultTheme.LocalSearchOpt
     | { provider: 'algolia'; options: DefaultTheme.AlgoliaSearchOptions }
 
 export const search: SearchConfig = {
-    provider: 'local'
+    provider: 'local',
+    options: {
+        translations: {
+            button: {
+                buttonText: '搜索',
+                buttonAriaLabel: '搜索'
+            },
+            modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                    selectText: '选择',
+                    navigateText: '切换'
+                }
+            }
+        }
+    }
 };
