dcd3d9eea035b79f2fb5ed147f28fff589047f0f
shenlink <2024133718@qq.com>
2024-12-02 18:02:29 +0800

    新增公安联网备案信息

--- docs/.vitepress/config/footer/footerConfig.ts
+++ docs/.vitepress/config/footer/footerConfig.ts
@@ -1,6 +1,17 @@
 import { DefaultTheme } from "vitepress";
 
 export const footer: DefaultTheme.Footer = {
-    message: '<a href="https://beian.miit.gov.cn/" style="text-decoration: none" target="_blank">粤ICP备2024331772号</a>',
+  message: `<div style="text-align: center;">
+    <a href="https://beian.miit.gov.cn/" style="text-decoration: none" target="_blank">
+      粤ICP备2024331772号
+    </a>
+    <br />
+    <div style="display: flex; justify-content: center; align-items: center; margin-top: 8px;">
+      <img src="/images/beian.png" alt="粤公网安备44060502003783" style="margin-right: 8px; width: 16px; height: 16px;">
+      <a href="https://beian.mps.gov.cn/#/query/webSearch?code=44060502003783" style="text-decoration: none" rel="noreferrer" target="_blank">
+        粤公网安备44060502003783
+      </a>
+    </div>
+  </div>`,
     copyright: 'Copyright © 2024-present <a href="https://github.com/shenlink">shenlink</a>'
 };
