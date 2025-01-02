7859ea51cf0308a3199f6abd3f7bc8d96008dc67
shenlink <2024133718@qq.com>
2025-01-03 02:26:22 +0800

    修改vite配置，加上@的目录路径别名，指向docs目录

--- docs/.vitepress/config/vite/viteConfig.ts
+++ docs/.vitepress/config/vite/viteConfig.ts
@@ -1,8 +1,14 @@
 import { fileWatcher } from "../../plugins/file-watcher"
 import { articlesDir } from '../extra/config'
+import path from "path"
 
 export const viteConfig = {
     plugins: [
         fileWatcher(articlesDir)
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, '../../../')
+        }
+    }
 }
